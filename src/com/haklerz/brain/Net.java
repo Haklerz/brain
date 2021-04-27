edf7dc493edbd6979e47c5d5f79d6a54626ffe28
Håkon Lervik <lervik.haakon@gmail.com>
2021-04-28 00:00:56 +0200

    Implementing update

--- src/com/haklerz/brain/Net.java
+++ src/com/haklerz/brain/Net.java
@@ -1,9 +1,57 @@
 package com.haklerz.brain;
 
+import java.util.Random;
+
 public class Net {
     private double[] nodes;
+    private WeightMatrix matrix;
 
-    public Net(int n) {
-        this.nodes = new double[n];
+    public Net(int nodeCount) {
+        this.nodes = new double[nodeCount];
+        this.matrix = WeightMatrix.newGaussian(nodeCount);
+
+        // Should be moved somewhere else.
+        Random random = new Random();
+        for (int i = 0; i < nodes.length; i++)
+            nodes[i] = random.nextGaussian();
+    }
+
+    public void update() {
+        double[] newNodes = new double[nodes.length];
+
+        for (int to = 0; to < newNodes.length; to++) {
+            double value = nodes[to];
+            for (int from = 0; from < newNodes.length; from++) {
+                newNodes[to] = value * matrix.getWeight(from, to);
+            }
+        }
+    }
+
+    public double[] getOutput(int count) {
+        if (count <= 0 || count > nodes.length)
+            throw new IndexOutOfBoundsException();
+
+        double[] output = new double[count];
+        for (int i = 0; i < count; i++)
+            output[i] = nodes[nodes.length - count + i];
+
+        return output;
+    }
+
+    public void setInput(double[] values) {
+        if (values.length > nodes.length)
+            throw new IllegalArgumentException();
+
+        for (int i = 0; i < values.length; i++)
+            nodes[i] = values[i];
+    }
+
+    public static void main(String[] args) {
+        Net net = new Net(5);
+        double[] out = net.getOutput(3);
+
+        for (int i = 0; i < out.length; i++) {
+            System.out.print(out[i] + ", ");
+        }
     }
 }
--- src/com/haklerz/brain/WeightMatrix.java
+++ src/com/haklerz/brain/WeightMatrix.java
@@ -3,36 +3,36 @@ package com.haklerz.brain;
 import java.util.Random;
 
 public class WeightMatrix {
-    private int size;
+    private final int nodeCount;
     private double[] weights;
 
-    public WeightMatrix(int n) {
-        this.size = n;
-        this.weights = new double[n * n];
+    public WeightMatrix(int nodeCount) {
+        this.nodeCount = nodeCount;
+        this.weights = new double[nodeCount * nodeCount];
     }
 
-    public double getWeight(int from, int to) {
-        if (from < 0 || from >= size || to < 0 || to >= size)
-            throw new IndexOutOfBoundsException();
-
-        return weights[from + to * size];
-    }
-
-    public void setWeight(int from, int to, double value) {
-        if (from < 0 || from >= size || to < 0 || to >= size)
-            throw new IndexOutOfBoundsException();
-        
-        weights[from + to * size] = value;
-    }
-
-    public static WeightMatrix gaussian(int n) {
-        WeightMatrix matrix = new WeightMatrix(n);
+    public static WeightMatrix newGaussian(int nodeCount) {
+        WeightMatrix matrix = new WeightMatrix(nodeCount);
         Random random = new Random();
-        
-        for (int i = 0; i < matrix.weights.length; i++) {
+
+        for (int i = 0; i < matrix.weights.length; i++)
             matrix.weights[i] = random.nextGaussian();
-        }
 
         return matrix;
     }
+
+    public double getWeight(int from, int to) {
+        if (from < 0 || from >= nodeCount || to < 0 || to >= nodeCount)
+            throw new IndexOutOfBoundsException();
+
+        return weights[from + to * nodeCount];
+    }
+
+    public void setWeight(int from, int to, double value) {
+        if (from < 0 || from >= nodeCount || to < 0 || to >= nodeCount)
+            throw new IndexOutOfBoundsException();
+
+        weights[from + to * nodeCount] = value;
+    }
+
 }
